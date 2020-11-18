9a28be66a23106cf859f648dd9ba09c8ad57e60f
TiagoBehenck <tiago.behenck.santos@gmail.com>
2020-11-17 22:38:51 -0300

    :new_moon: Add dark theme

---
+++ .storybook/Theme.js
@@ -0,0 +1,28 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+
+  colorSecondary: "#7159c1",
+
+  // UI
+  appBg: "#0b0a0d",
+  appContentBg: "#16161A",
+  appBorderColor: "#7159c1",
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+
+  // Text colors
+  textColor: "#fff",
+
+  // Toolbar default and active colors
+  barTextColor: "#e1e1e1",
+  barSelectedColor: "#fff",
+  barBg: "#1b1a1f",
+
+  brandTitle: "UI Story Book",
+  brandUrl: "https://example.com",
+});
